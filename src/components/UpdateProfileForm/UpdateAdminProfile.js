1aabad188abfb52e6f80f8e5688dcc252c4f9cd1
tracy zumbu garcia <tracyzumbu@lilo.org>
2020-11-24 19:24:43 +0100

    added headers and then and catch to axios.put for updateProfile route

--- src/components/UpdateProfileForm/UpdateAdminProfile.js
+++ src/components/UpdateProfileForm/UpdateAdminProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
@@ -10,11 +10,13 @@ import decode from "jwt-decode";
 
 const UpdateAdminProfile = (props) => {
   //get the user's id form localstorage
-  const token = localStorage.getItem("token")
-  const {userID} = decode(token)
-  
-// get user's details from userProfile component
-  const userDetails = props.userDetails
+  const token = localStorage.getItem("token");
+  const { userID } = decode(token);
+
+  const [response, setResponse] = useState();
+
+  // get user's details from userProfile component
+  const userDetails = props.userDetails;
   //set the values of the form with userDetails content
   const values = {
     first_name: userDetails.first_name,
@@ -22,8 +24,8 @@ const UpdateAdminProfile = (props) => {
     email: userDetails.email,
     phone: userDetails.phone,
     logo: userDetails.logo,
-    password:"",
-     repeat_password:""
+    password: "",
+    repeat_password: "",
   };
   //set rules for validating of the field's form
   const validationSchema = Yup.object({
@@ -40,15 +42,31 @@ const UpdateAdminProfile = (props) => {
   });
 
   const onSubmit = async (values) => {
-    delete values["repeat_password"]
+    delete values["repeat_password"];
     //remove empty string from the objects "values" in order to add into the BDD only values' fields provided
     Object.keys(values).forEach(
       (key) => values[key] === "" && delete values[key]
-    );
+      );
+      
+      console.log('values:', values)
     const url = `http://localhost:4040/allpeople/updateProfile/${userID}`;
-    await axios.put(url, values);
+    await axios({
+      method: "PUT",
+      url: url,
+      data: values,
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token,
+      },
+    })
+      .then((res) => setResponse(res.data))
+      .catch((error) => {
+        console.log(error);
+      });
     //window.location.reload()
   };
+
+  
   return (
     <div>
       {!userDetails.logo ? null : (
@@ -58,61 +76,64 @@ const UpdateAdminProfile = (props) => {
           onSubmit={onSubmit}
           enableReinitialize
         >
-          {(formik) => (
-            <Form className="signIn__form">
-              <FormikControl
-                control="input"
-                type="text"
-                name="first_name"
-                label="Prénom"
-              />
-              <FormikControl
-                control="input"
-                type="text"
-                name="last_name"
-                label="Nom"
-              />
-              <FormikControl
-                control="input"
-                type="email"
-                name="email"
-                label="E-mail"
-              />
-              <FormikControl
-                control="input"
-                type="password"
-                name="password"
-                label="mot de passe"
-                placeholder="********"
-              />
-              <FormikControl
-                control="input"
-                type="password"
-                name="repeat_password"
-                label="répétez votre mot de passe"
-                placeholder="********"
-              />
-              <FormikControl
-                control="input"
-                type="tel"
-                name="phone"
-                label="Téléphone"
-              />
-              <FormikControl
-                control="input"
-                type="text"
-                name="logo"
-                label="Photo"
-              />
+          {(formik) => {
+            console.log("formik:", formik);
+            return (
+              <Form className="signIn__form">
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="first_name"
+                  label="Prénom"
+                />
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="last_name"
+                  label="Nom"
+                />
+                <FormikControl
+                  control="input"
+                  type="email"
+                  name="email"
+                  label="E-mail"
+                />
+                <FormikControl
+                  control="input"
+                  type="password"
+                  name="password"
+                  label="mot de passe"
+                  placeholder="********"
+                />
+                <FormikControl
+                  control="input"
+                  type="password"
+                  name="repeat_password"
+                  label="répétez votre mot de passe"
+                  placeholder="********"
+                />
+                <FormikControl
+                  control="input"
+                  type="tel"
+                  name="phone"
+                  label="Téléphone"
+                />
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="logo"
+                  label="Photo"
+                />
 
-              <Button
-                type="submit"
-                disabled={!(formik.dirty && formik.isValid)}
-                className={"btn btn--round"}
-                value={"Modifier mon profil"}
-              />
-            </Form>
-          )}
+                <Button
+                  type="submit"
+                  disabled={!(formik.dirty && formik.isValid)}
+                  className={"btn btn--round"}
+                  value={"Modifier mon profil"}
+                />
+              </Form>
+            );
+          }}
         </Formik>
       )}
     </div>
